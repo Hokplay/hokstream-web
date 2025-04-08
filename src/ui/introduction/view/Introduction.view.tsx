f17d9084e891f5193675bd1e13932aa008c0edae
yachen168 <io260188@gmail.com>
2025-04-09 00:02:47 +0800

    feat: add core value section

--- src/ui/introduction/view/Introduction.view.tsx
+++ src/ui/introduction/view/Introduction.view.tsx
@@ -1,11 +1,15 @@
 import Banner from '@/ui/introduction/Banner/Banner.tsx';
+import CoreValue from '@/ui/introduction/CoreValue/CoreValue.tsx';
 
 import './Introduction.scss';
 
 function IntroductionView() {
   return (
     <div>
-      <Banner />
+      <div x-class="max-w-1200px mx-auto">
+        <Banner />
+        <CoreValue />
+      </div>
     </div>
   );
 }
