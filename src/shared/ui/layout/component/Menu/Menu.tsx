1988e9860e844d2584233881ad3db48ee48307b0
yachen168 <io260188@gmail.com>
2025-04-08 22:47:25 +0800

    feat: highlight current menu item

--- src/shared/ui/layout/component/Menu/Menu.tsx
+++ src/shared/ui/layout/component/Menu/Menu.tsx
@@ -1,11 +1,12 @@
 import { RoutePath } from '@/ui/route/RoutePath.ts';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useTranslation } from '@/i18n/hook/useTranslation.ts';
 
 import './Menu.scss';
 
 function Menu() {
   const { t } = useTranslation();
+  const location = useLocation();
 
   const menuList = [
     {
@@ -29,7 +30,16 @@ function Menu() {
   return (
     <nav uno-flex="~ gap-25px">
       {menuList.map(menu => (
-        <Link key={menu.to} to={menu.to} x-class="menu-item">
+        <Link
+          key={menu.to}
+          to={menu.to}
+          x-class={[
+            'menu-item',
+            {
+              highlight: location.pathname === menu.to
+            }
+          ]}
+        >
           {menu.label}
         </Link>
       ))}
