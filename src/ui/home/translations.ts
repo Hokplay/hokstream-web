2aa54ec29050f2fa2d150a7f90066075b8f8bb2c
yachen168 <io260188@gmail.com>
2025-04-08 22:17:53 +0800

    refactor: translations.ts

---
+++ src/ui/home/translations.ts
@@ -0,0 +1,34 @@
+export const translations = {
+  'zh-TW': {
+    sportsmeshTitle: 'SPORTSMESH',
+    realTimeVideo: '即時視頻',
+    aiAnalysis: 'AI 分析',
+    game: '遊戲',
+    socialMedia: '社交媒體',
+    dataAnalysis: '數據分析',
+    fan: '粉絲',
+    contactBtn: '聯絡我們',
+    howItWorks: 'SPORTSMESH如何運作?',
+    traditional: '傳統製作',
+    traditionalDesc: '單向傳輸，額外從體影平台取得素材，浪費無謂的時間',
+    social: '社群製作',
+    socialDesc: '多向傳訊製作素材，不需距離影響，達到全中心化效果'
+  },
+  'en-US': {
+    sportsmeshTitle: 'SPORTSMESH',
+    realTimeVideo: 'Real-time Video',
+    aiAnalysis: 'AI Analysis',
+    game: 'Game',
+    socialMedia: 'Social Media',
+    dataAnalysis: 'Data Analysis',
+    fan: 'Fan',
+    contactBtn: 'Contact Us',
+    howItWorks: 'How SPORTSMESH Works?',
+    traditional: 'Traditional Production',
+    traditionalDesc:
+      'One-way transmission, requiring extra materials from streaming platforms, wasting unnecessary time',
+    social: 'Social Production',
+    socialDesc:
+      'Multi-directional transmission for creating materials, no distance impact, achieving fully centralized effect'
+  }
+};
--- src/ui/home/view/Home.view.tsx
+++ src/ui/home/view/Home.view.tsx
@@ -2,43 +2,9 @@ import { useTranslation } from '@/i18n/hook/useTranslation.ts';
 import Tags from '@/ui/home/component/Tags/Tags.tsx';
 import Partners from '@/ui/home/component/Partners/Partners.tsx';
 import ContactUsButton from '@/shared/ui/component/ContactUsButton/ContactUsButton.tsx';
+import { translations } from '@/ui/home/translations.ts';
 
 import './Home.scss';
-
-const translations = {
-  'zh-TW': {
-    sportsmeshTitle: 'SPORTSMESH',
-    realTimeVideo: '即時視頻',
-    aiAnalysis: 'AI 分析',
-    game: '遊戲',
-    socialMedia: '社交媒體',
-    dataAnalysis: '數據分析',
-    fan: '粉絲',
-    contactBtn: '聯絡我們',
-    howItWorks: 'SPORTSMESH如何運作?',
-    traditional: '傳統製作',
-    traditionalDesc: '單向傳輸，額外從體影平台取得素材，浪費無謂的時間',
-    social: '社群製作',
-    socialDesc: '多向傳訊製作素材，不需距離影響，達到全中心化效果'
-  },
-  'en-US': {
-    sportsmeshTitle: 'SPORTSMESH',
-    realTimeVideo: 'Real-time Video',
-    aiAnalysis: 'AI Analysis',
-    game: 'Game',
-    socialMedia: 'Social Media',
-    dataAnalysis: 'Data Analysis',
-    fan: 'Fan',
-    contactBtn: 'Contact Us',
-    howItWorks: 'How SPORTSMESH Works?',
-    traditional: 'Traditional Production',
-    traditionalDesc:
-      'One-way transmission, requiring extra materials from streaming platforms, wasting unnecessary time',
-    social: 'Social Production',
-    socialDesc:
-      'Multi-directional transmission for creating materials, no distance impact, achieving fully centralized effect'
-  }
-};
 
 function HomeView() {
   const { locale } = useTranslation();
