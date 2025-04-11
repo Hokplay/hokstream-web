9f4cd898f53623af07c9d8551e851db826dfdcf4
yachen168 <yachen@hokstream.com>
2025-04-11 18:40:22 +0800

    feat: add container for CapabilitiesCarousel

--- src/ui/successCase/component/CapabilitiesCarousel/CapabilitiesCarousel.tsx
+++ src/ui/successCase/component/CapabilitiesCarousel/CapabilitiesCarousel.tsx
@@ -102,41 +102,43 @@ function CapabilitiesCarousel() {
   const sliderList = translations[locale];
 
   return (
-    <Slider {...settings}>
-      {sliderList.map((slider, index) => (
-        <div key={index} x-class="md-px-10px">
-          <div
-            x-class="relative min-h-240px aspect-16/9 overflow-hidden"
-            uno-border="rounded-20px"
-          >
-            <img
-              x-class="w-full h-full object-cover"
-              src={slider.image}
-              alt={slider.title}
-            />
+    <section x-class="pb-120px">
+      <Slider {...settings} x-class="pb-15px">
+        {sliderList.map((slider, index) => (
+          <div key={index} x-class="md-px-10px">
             <div
-              x-class="absolute h-full w-120px md-w-150px lg-w-200px md-px-20px left-0 top-0 lg-py-40px bg-white/60 backdrop-blur-[20px]"
-              uno-p="x-10px y-20px"
+              x-class="relative min-h-240px aspect-16/9 overflow-hidden"
+              uno-border="rounded-20px"
             >
-              <h3 x-class="my-0 lg-text-24px" uno-text="20px $Primary">
-                {slider.title}
-              </h3>
-              <ul x-class="pl-20px">
-                {slider.list.map((listItem, i) => (
-                  <li
-                    key={i}
-                    uno-text="12px"
-                    x-class="md-text-16px lg-text-20px"
-                  >
-                    {listItem}
-                  </li>
-                ))}
-              </ul>
+              <img
+                x-class="w-full h-full object-cover"
+                src={slider.image}
+                alt={slider.title}
+              />
+              <div
+                x-class="absolute h-full w-120px md-w-150px lg-w-200px md-px-20px left-0 top-0 lg-py-40px bg-white/60 backdrop-blur-[20px]"
+                uno-p="x-10px y-20px"
+              >
+                <h3 x-class="my-0 lg-text-24px" uno-text="20px $Primary">
+                  {slider.title}
+                </h3>
+                <ul x-class="pl-20px">
+                  {slider.list.map((listItem, i) => (
+                    <li
+                      key={i}
+                      uno-text="12px"
+                      x-class="md-text-16px lg-text-20px"
+                    >
+                      {listItem}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </Slider>
+        ))}
+      </Slider>
+    </section>
   );
 }
 
