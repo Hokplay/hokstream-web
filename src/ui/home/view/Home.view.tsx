9e6978998577cd317360c7254f185a4d8b0623c6
yachen-hokstream <yachen@hokstream.com>
2025-04-13 19:23:44 +0800

    fix: partners section (#23)
    * fix: layout of partners section
    
    * refactor: remove redundant css
    
    * fix: padding

--- src/ui/home/view/Home.view.tsx
+++ src/ui/home/view/Home.view.tsx
@@ -10,54 +10,55 @@ function HomeView() {
   const t = translations[locale];
 
   return (
-    <div className="intro-page px-20px lg-px-0 max-w-1200px mx-auto">
-      <Banner title={t.sportsmeshTitle} />
-      <section className="how-it-works">
-        <h2>{t.howItWorks}</h2>
+    <>
+      <div className="intro-page px-20px xl-px-0 max-w-1200px mx-auto">
+        <Banner title={t.sportsmeshTitle} />
+        <section className="how-it-works">
+          <h2>{t.howItWorks}</h2>
 
-        <div className="md-flex-row" uno-flex="~ col items-center gap-30px">
-          <div className="traditional bg-#ffffff">
-            <h3>{t.traditional}</h3>
-            <p>{t.traditionalDesc}</p>
-            <div className="flow-diagram">
-              <img
-                x-if={locale === 'zh-TW'}
-                src="/images/home/ZH_traditional_brocasting.png"
-                alt="傳統製作流程"
-              />
-              <img
-                x-if={locale === 'en-US'}
-                src="/images/home/EN_tradtional.png"
-                alt="傳統製作流程"
-              />
+          <div className="md-flex-row" uno-flex="~ col items-center gap-30px">
+            <div className="traditional bg-#ffffff">
+              <h3>{t.traditional}</h3>
+              <p>{t.traditionalDesc}</p>
+              <div className="flow-diagram">
+                <img
+                  x-if={locale === 'zh-TW'}
+                  src="/images/home/ZH_traditional_brocasting.png"
+                  alt="傳統製作流程"
+                />
+                <img
+                  x-if={locale === 'en-US'}
+                  src="/images/home/EN_tradtional.png"
+                  alt="傳統製作流程"
+                />
+              </div>
+            </div>
+
+            <div className="vs">VS</div>
+
+            <div className="social bg-#ffffff">
+              <h3>{t.social}</h3>
+              <p>{t.socialDesc}</p>
+              <div className="flow-diagram">
+                <img
+                  x-if={locale === 'zh-TW'}
+                  src="/images/home/ZH_social_broadcasting.png"
+                  alt="社群製作流程"
+                />
+                <img
+                  x-if={locale === 'en-US'}
+                  src="/images/home/EN_social.png"
+                  alt="Community production"
+                />
+              </div>
             </div>
           </div>
-
-          <div className="vs">VS</div>
-
-          <div className="social bg-#ffffff">
-            <h3>{t.social}</h3>
-            <p>{t.socialDesc}</p>
-            <div className="flow-diagram">
-              <img
-                x-if={locale === 'zh-TW'}
-                src="/images/home/ZH_social_broadcasting.png"
-                alt="社群製作流程"
-              />
-              <img
-                x-if={locale === 'en-US'}
-                src="/images/home/EN_social.png"
-                alt="Community production"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="partners">
+        </section>
+      </div>
+      <section className="p-[40px_0_100px_0]">
         <Partners />
       </section>
-    </div>
+    </>
   );
 }
 
