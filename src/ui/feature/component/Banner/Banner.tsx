4e3d4fee85045508be35bb44a85027812269e397
yachen168 <yachen@hokstream.com>
2025-04-15 16:12:20 +0800

    fix: replace bg-img with img

--- src/ui/feature/component/Banner/Banner.tsx
+++ src/ui/feature/component/Banner/Banner.tsx
@@ -6,10 +6,14 @@ interface BannerProps {
 function Banner({ title, image }: BannerProps) {
   return (
     <div
-      x-class={`relative lg-block h-85vh lg-h-auto lg-pt-60px bg-[url(${image})] lg-bg-none overflow-hidden items-center`}
-      uno-bg="no-repeat [position:60%_50%] cover"
+      x-class={`relative lg-block h-85vh lg-h-auto lg-pt-60px overflow-hidden items-center`}
       uno-flex="~"
     >
+      <img
+        x-class="lg-hidden absolute w-full h-full object-cover object-[65%_50%] h-full"
+        src={image}
+        alt={title}
+      />
       <div x-class="absolute w-120% left-[-42%] aspect-1/1 top-1/2 translate-y--1/2 bg-$Base opacity-90 rounded-full lg-hidden" />
       <h1 className="my-0 xl-text-96px z-2 pl-10px max-w-60%" uno-text="64px">
         {title}
