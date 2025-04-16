0f6005219aff9c37aa5cc0570e4ec0c8015d13c4
yachen-hokstream <yachen@hokstream.com>
2025-04-16 23:01:49 +0800

    Revert "refactor: redefine translation data structure and split components (#31)"
    This reverts commit 10ecb57f28eeebcab6bd0db8e55b9e27da07fadb.

--- src/ui/feature/translation/translations.ts
+++
@@ -1,7 +0,0 @@
-import { enUsTranslation } from './enUsTranslation.ts';
-import { zhTwTranslation } from './zhTwTranslation.ts';
-
-export const translations = {
-  'en-US': enUsTranslation,
-  'zh-TW': zhTwTranslation
-};
