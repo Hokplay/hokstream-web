d622a9efa877a7926ae1f15f084451ed1abd27a6
yachen168 <yachen@hokstream.com>
2025-04-17 14:44:09 +0800

    refactor: use grid

--- src/ui/introduction/Banner/Banner.tsx
+++ src/ui/introduction/Banner/Banner.tsx
@@ -1,16 +1,24 @@
 import type { BannerSection } from '@/ui/introduction/translation/IntroductionPageTranslation.ts';
+import Row from '@/shared/ui/component/Row/Row.tsx';
+import Col from '@/shared/ui/component/Col/Col.tsx';
 
 interface BannerProps {
   data: BannerSection;
 }
 function Banner({ data }: BannerProps) {
   return (
-    <section x-class="py-30px md-flex-row" uno-flex="~ col justify-between">
-      <h2 x-class="text-50px md-text-60px lg-text-96px">{data.title}</h2>
-      <div x-class="w-full md-w-65% aspect-845/644 rounded-20px overflow-hidden">
-        <img x-class="w-full h-full" src={data.image} alt={data.title} />
-      </div>
-    </section>
+    <Row x-class="py-30px" align="middle">
+      <Col xs={24} md={10}>
+        <h2 x-class="my-0 mb-10px text-50px md-text-60px lg-text-96px">
+          {data.title}
+        </h2>
+      </Col>
+      <Col xs={24} md={14}>
+        <div x-class="aspect-845/644 rounded-20px overflow-hidden">
+          <img x-class="w-full h-full" src={data.image} alt={data.title} />
+        </div>
+      </Col>
+    </Row>
   );
 }
 
