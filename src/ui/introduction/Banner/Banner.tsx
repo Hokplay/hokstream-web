ab6dd4ca89f9bb39eba79c26d13dcb46f41dd16b
yachen168 <yachen@hokstream.com>
2025-04-18 14:57:25 +0800

    feat: add sportsmesh video

--- src/ui/introduction/Banner/Banner.tsx
+++ src/ui/introduction/Banner/Banner.tsx
@@ -14,8 +14,17 @@ function Banner({ data }: BannerProps) {
         </h2>
       </Col>
       <Col xs={24} md={14}>
-        <div x-class="aspect-845/644 rounded-20px overflow-hidden">
-          <img x-class="w-full h-full" src={data.image} alt={data.title} />
+        <div x-class="aspect-16/9 rounded-20px overflow-hidden">
+          <iframe
+            uno-border="none"
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/JWJCzhxRZFw?si=-hzC9VwYYIuXTjEw"
+            title="SportsMesh"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
         </div>
       </Col>
     </Row>
